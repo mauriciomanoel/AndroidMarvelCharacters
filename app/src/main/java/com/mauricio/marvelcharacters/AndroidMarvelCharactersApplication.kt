e1907b8af8373c051466ec40b1d54d03ab1937f6
Mauricio Manoel <mauriciomanoel@gmail.com>
2022-02-26 19:21:01 +0000

    add dependencies in the project

---
+++ app/src/main/java/com/mauricio/marvelcharacters/AndroidMarvelCharactersApplication.kt
@@ -0,0 +1,7 @@
+package com.mauricio.marvelcharacters
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AndroidMarvelCharactersApplication: Application()
