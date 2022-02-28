d01b4602534c2b906fb1d5a954e0d4bbec91d96f
Mauricio Manoel <mauriciomanoel@gmail.com>
2022-02-28 22:56:58 +0000

    Add support to Pagination using Android Paging

--- app/src/main/java/com/mauricio/marvel/characters/viewmodels/CharacterViewModel.kt
+++ app/src/main/java/com/mauricio/marvel/characters/viewmodels/CharacterViewModel.kt
@@ -12,25 +12,7 @@ import javax.inject.Inject
 @HiltViewModel
 class CharacterViewModel @Inject constructor(val repository: CharacterRepository) : ViewModel() {
 
-    private val _showLoading = MutableLiveData<Boolean>()
-    val showLoading: LiveData<Boolean> = _showLoading
-
-    private val _messageError = MutableLiveData<String>()
-    val messageError: LiveData<String> = _messageError
-
     fun charactersInSeries() = repository.getCharactersInSeries().cachedIn(viewModelScope)
     fun charactersEvents(characterId:Long) = repository.getCharacterEvents(characterId).cachedIn(viewModelScope)
-
-    private fun showLoading() {
-        _showLoading.apply {
-            postValue(true)
-        }
-    }
-
-    private fun hideLoading() {
-        _showLoading.apply {
-            postValue(false)
-        }
-    }
 
 }
